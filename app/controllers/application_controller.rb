696932937ad82a4cc2dd14ba18bc48972dc79c3c
jo_gus <jo.gu@dji.com>
2016-07-26 14:50:46 +0800

    Implememt basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "hello, world!"
-  end
-
-  def full_text(title)
-    if title == nil or title.empty?
-      return 'Ruby on Rails Tutorial Sample App'
-    else
-      return ['title', '|', 'Ruby on Rails Tutorial Sample App'].join
-    end
-  end
+  include SessionsHelper
 end
