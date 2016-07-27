60a717143b3b1a8407543583c0d6e9f22fc80b57
jo_gus <jo.gu@dji.com>
2016-07-27 15:31:35 +0800

    modify

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
       login_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       #redirect_to user
-      debugger
       redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
